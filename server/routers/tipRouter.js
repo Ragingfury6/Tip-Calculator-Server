d547d85fcdad3a2f637d53c7ae381b30779fd76d
Ragingfury6 <trentsuperman@gmail.com>
2023-07-14 16:14:19 -0700

    Search Feature Started

--- server/routers/tipRouter.js
+++ server/routers/tipRouter.js
@@ -2,6 +2,7 @@ import express from 'express';
 import Tip from '../database/models/Tip.js';
 const tipRouter = express.Router();
 
+// Get all Tips
 tipRouter.get("/", async (req,res)=>{
     const allTips = await Tip.find();
     if(allTips){
@@ -10,12 +11,14 @@ tipRouter.get("/", async (req,res)=>{
     return res.sendStatus(404);
 });
 
+// Get Specific Tip By Id
 tipRouter.get("/:id", async (req, res) => {
     const {id} = req.params;
     const tip = await Tip.findById(id);
     return res.status(200).json(tip);
 });
 
+// Add a tip
 tipRouter.post("/", async (req,res) => {
     const {location, total, employees, tips, hours} = req.body;
     const date = new Date();
@@ -27,6 +30,14 @@ tipRouter.post("/", async (req,res) => {
         }
         return res.sendStatus(400);
     }
-})
+});
+
+// Get tips based on filters
+tipRouter.post("/filter", async (req,res)=>{
+    const {location, startDate, endDate, startTime, endTime, minTotal, maxTotal} = req.body;
+    // "build" a Date from date and location to query the db
+    const filteredTips = await Tip.find({})
+});
+
 
 export default tipRouter;
