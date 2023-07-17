656570dfd4d80f67fe5cc4ca4685ec45206dfbdf
Ragingfury6 <trentsuperman@gmail.com>
2023-07-16 22:37:03 -0700

    new route

--- server/routers/tipRouter.js
+++ server/routers/tipRouter.js
@@ -1,43 +1,57 @@
-import express from 'express';
-import Tip from '../database/models/Tip.js';
+import express from "express";
+import Tip from "../database/models/Tip.js";
 const tipRouter = express.Router();
 
 // Get all Tips
-tipRouter.get("/", async (req,res)=>{
-    const allTips = await Tip.find();
-    if(allTips){
-        return res.status(200).json(allTips);
-    }
-    return res.sendStatus(404);
+tipRouter.get("/", async (req, res) => {
+  const allTips = await Tip.find();
+  if (allTips) {
+    return res.status(200).json(allTips);
+  }
+  return res.sendStatus(404);
 });
 
 // Get Specific Tip By Id
 tipRouter.get("/:id", async (req, res) => {
-    const {id} = req.params;
-    const tip = await Tip.findById(id);
-    return res.status(200).json(tip);
+  const { id } = req.params;
+  const tip = await Tip.findById(id);
+  return res.status(200).json(tip);
 });
 
 // Add a tip
-tipRouter.post("/", async (req,res) => {
-    const {location, total, employees, tips, hours} = req.body;
-    const date = new Date();
-    if(date && location && total && employees && tips && hours){
-        const newTip = new Tip({date, location, total, employees, tips, hours});
-        const insertedTip = await newTip.save();
-        if(insertedTip){
-            return res.status(201).json(insertedTip);
-        }
-        return res.sendStatus(400);
+tipRouter.post("/", async (req, res) => {
+  const { location, total, employees, tips, hours } = req.body;
+  const date = new Date();
+  if (date && location && total && employees && tips && hours) {
+    const newTip = new Tip({ date, location, total, employees, tips, hours });
+    const insertedTip = await newTip.save();
+    if (insertedTip) {
+      return res.status(201).json(insertedTip);
     }
+    return res.sendStatus(400);
+  }
 });
 
 // Get tips based on filters
-tipRouter.post("/filter", async (req,res)=>{
-    const {location, startDate, endDate, startTime, endTime, minTotal, maxTotal} = req.body;
-    // "build" a Date from date and location to query the db
-    const filteredTips = await Tip.find({})
+tipRouter.post("/filter", async (req, res) => {
+  const {
+    location,
+    startDate,
+    endDate,
+    startTime,
+    endTime,
+    minTotal,
+    maxTotal,
+  } = req.body;
+  // "build" a Date from date and location to query the db
+  const filteredTips = await Tip.find({
+    location: { $regex: location, $options: "i" },
+    total: {$gte:minTotal, $lte:maxTotal}
+  });
+  if(filteredTips){
+    return res.status(200).json(filteredTips);
+  }
+  return res.sendStatus(400);
 });
-
 
 export default tipRouter;
