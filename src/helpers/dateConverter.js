1433350a0d5b08011e718d3dcf308a1125d816e8
Ragingfury6 <trentsuperman@gmail.com>
2023-07-16 22:15:54 -0700

    Modules Back

--- src/helpers/dateConverter.js
+++ src/helpers/dateConverter.js
@@ -1,11 +1,9 @@
-// import converter from 'number-to-words';
-// import convertTime from 'convert-time';
+import converter from 'number-to-words';
+import convertTime from 'convert-time';
 const MONTHS = ['Jan','Feb','Mar','Apr','May','Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 export default function dateConverter(str){
     const [date, time] = str.split(",");
     const month = MONTHS[Number(date.substr(3,2))-1];
-    // const day = converter.toOrdinal(Number(date.substr(0,2)));
-    const day = Number(date.substr(0,2));
-    return {month, day, time}
-    // return {month, day, time: convertTime(time, 'hh:MM A')}
+    const day = converter.toOrdinal(Number(date.substr(0,2)));
+    return {month, day, time: convertTime(time, 'hh:MM A')}
 }
